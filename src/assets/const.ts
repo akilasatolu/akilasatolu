28f445db4dd6c70bd115ca8891fd581834600945
akilasatolu <akilasatolu@gmail.com>
2025-07-02 17:26:42 +0900

    add const.ts

---
+++ src/assets/const.ts
@@ -0,0 +1,20 @@
+const repositoryName = '/akilasatolu/';
+
+export const pageInfo = {
+  "Home": {
+    "to": repositoryName + "",
+    "text": "about"
+  },
+  "Experience": {
+    "to": repositoryName + "experience/",
+    "text": "experience"
+  },
+  "Works": {
+    "to": repositoryName + "works/",
+    "text": "works"
+  },
+  "Labo": {
+    "to": repositoryName + "labo/",
+    "text": "labo"
+  }
+};
